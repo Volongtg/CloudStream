867737ee3c90c96e420beb962def15daa1a57f00
Volongtg <158327191+Volongtg@users.noreply.github.com>
2026-08-14 18:47:48 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.2.2")
         // Pin the CloudStream Gradle plugin to a fixed JitPack commit.
         // The old master-SNAPSHOT was non-reproducible and was the source
         // of the failing configuration on GitHub Actions.
-        classpath("com.github.recloudstream:gradle:7116299")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
@@ -42,20 +42,20 @@ subprojects {
     }
 
     android {
-        compileSdkVersion(33)
+        compileSdkVersion(35)
         defaultConfig {
             minSdk = 21
-            targetSdk = 33
+            targetSdk = 35
         }
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
     }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "1.8"
+            jvmTarget = "17"
             freeCompilerArgs = freeCompilerArgs + listOf(
                 "-Xno-call-assertions",
                 "-Xno-param-assertions",
@@ -68,7 +68,7 @@ subprojects {
         val apk by configurations
         val implementation by configurations
         apk("com.lagradost:cloudstream3:pre-release")
-        implementation(kotlin("stdlib"))
+        implementation(kotlin("stdlib", "2.3.0"))
         implementation("com.github.Blatzar:NiceHttp:0.3.5")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
         implementation("org.jsoup:jsoup:1.13.1")
