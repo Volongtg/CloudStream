38979c2e272010627c246ae9ee899b77f098c68a
Volongtg <158327191+Volongtg@users.noreply.github.com>
2026-08-14 15:56:19 +0700

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        // Pin the CloudStream Gradle plugin to a fixed JitPack commit.
+        // The old master-SNAPSHOT was non-reproducible and was the source
+        // of the failing configuration on GitHub Actions.
+        classpath("com.github.recloudstream:gradle:7116299")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "local/HHKUNGFU-CloudStream")
+    }
+
+    android {
+        compileSdkVersion(30)
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 30
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-Xno-call-assertions",
+                "-Xno-param-assertions",
+                "-Xno-receiver-assertions"
+            )
+        }
+    }
+
+    dependencies {
+        val apk by configurations
+        val implementation by configurations
+        apk("com.lagradost:cloudstream3:pre-release")
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.3.2")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        implementation("org.jsoup:jsoup:1.13.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+        implementation("org.mozilla:rhino:1.7.14")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
