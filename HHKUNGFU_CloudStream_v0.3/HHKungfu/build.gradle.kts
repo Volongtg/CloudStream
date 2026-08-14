e8eb97d086f8952e9a70848a105b1d3219ef24aa
Volongtg <158327191+Volongtg@users.noreply.github.com>
2026-08-14 15:04:41 +0700

    Delete HHKUNGFU_CloudStream_v0.3 directory

--- HHKUNGFU_CloudStream_v0.3/HHKungfu/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-cloudstream {
-    description = "Provider CloudStream cho HHKUNGFU"
-    authors = listOf("Vo Long")
-    status = 1
-    tvTypes = listOf("Anime", "AnimeMovie", "TvSeries", "Movie")
-    language = "vi"
-    iconUrl = "https://hhkungfu.ee/favicon.ico"
-}
